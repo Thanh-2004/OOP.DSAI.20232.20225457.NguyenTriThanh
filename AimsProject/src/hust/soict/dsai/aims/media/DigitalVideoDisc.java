ceebbca2036414431598049838d86f1dbef17eda
Thanh-2004 <thanh.20040803@gmail.com>
2024-05-18 17:19:59 +0700

    update class diagram lab04

--- AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -1,86 +1,51 @@
 package hust.soict.dsai.aims.media;
 
-public class DigitalVideoDisc {
-	private String title;
-	private String category;
-	private String director;
-	private int length;
-	private float cost;
-	private int id;
-	
-	private static int nbDigitalVideoDiscs = 0;
-	
-	public String getTitle() {
-		return title;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public String getDirector() {
-		return director;
-	}
-	public int getLength() {
-		return length;
-	}
-	public float getCost() {
-		return cost;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
+public class DigitalVideoDisc extends Disc implements Playable{
+
+//	private String director;
+//	private int length;
 
 	
-	public DigitalVideoDisc() {
-		nbDigitalVideoDiscs += 1;
-		this.id = nbDigitalVideoDiscs;
-	}
+	private static int nbDigitalVideoDiscs = 0;
+
 	
+	
+	
+	
+//	public DigitalVideoDisc() {
+//		super();
+//		nbDigitalVideoDiscs += 1;
+//		setId(nbDigitalVideoDiscs);
+//	}
+
 	public DigitalVideoDisc(String title) {
-		super();
-		this.title = title;
+		super(title);
 		nbDigitalVideoDiscs += 1;
-		this.id = nbDigitalVideoDiscs;
+		setId(nbDigitalVideoDiscs);
 	}
 	
 	public DigitalVideoDisc(String category, String title, float cost) {
-		super();
-		this.category = category;
-		this.title = title;
-		this.cost = cost;
+		super(category, title, cost);
 		nbDigitalVideoDiscs += 1;
-		this.id = nbDigitalVideoDiscs;
+		setId(nbDigitalVideoDiscs);
 	}
 	
 	public DigitalVideoDisc(String director, String category, String title, float cost) {
-		super();
-		this.director = director;
-		this.category = category;
-		this.title = title;
-		this.cost = cost;
+		super(director,category, title, cost);
 		nbDigitalVideoDiscs += 1;
-		this.id = nbDigitalVideoDiscs;
+		setId(nbDigitalVideoDiscs);
 	}
 	
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-		super();
-		this.director = director;
-		this.category = category;
-		this.length = length;
-		this.title = title;
-		this.cost = cost;
+		super(title, category, director, length , cost);
 		nbDigitalVideoDiscs += 1;
-		this.id = nbDigitalVideoDiscs;
+		setId(nbDigitalVideoDiscs);
 	}
 	
-	public String toString() {
-		return "DVD - " + this.getTitle() + " - " + this.getCategory() + " - " + this.getDirector() + " - " + this.getLength() + ": " + this.getCost() + " $" ;
-	}
+	
+//	public String toString() {
+//		return "DVD - " + this.getTitle() + " - " + this.getCategory() + " - " + this.getDirector() + " - " + this.getLength() + ": " + this.getCost() + " $" ;
+//	}
 	
 	public boolean isMatch(String title) {
 		String lowercaseTitle = title.toLowerCase();
@@ -99,5 +64,10 @@ public class DigitalVideoDisc {
 			return true;
 		}
 		return false;
+	}
+	
+	public void play() {
+		System.out.println("Playing DVD: " + this.getTitle());
+		System.out.println("DVD length: " + this.getLength());
 	}
 }
--- AimsProject/src/hust/soict/dsai/aims/media/Track.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Track.java
@@ -1,6 +1,6 @@
 package hust.soict.dsai.aims.media;
 
-public class Track {
+public class Track implements Playable{
 	private String title;
 	private int length;
 	public Track(String title, int length) {
